dfd6440825c41c815aa0c3b6a69c9d38f516989d
Splix1 <unversed1241@yahoo.com>
2025-02-13 10:05:23 -0800

    re-generated frontend with Supabase & added token validation in .NET

--- backend/noDoom/Program.cs
+++ backend/noDoom/Program.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Supabase;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 string supabaseUrl = builder.Configuration["Supabase:Url"];
 string supabaseKey = builder.Configuration["Supabase:Key"];
+string jwtSecretKey = builder.Configuration["Supabase:JWT-secret"];
 
 
 // Add services to the container.
@@ -21,6 +25,25 @@ await supabaseClient.InitializeAsync();
 
 builder.Services.AddSingleton(supabaseClient);
 
+
+// Add authentication
+
+var bytes = Encoding.UTF8.GetBytes(jwtSecretKey!);
+
+builder.Services.AddAuthentication().AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = false,
+        IssuerSigningKey = new SymmetricSecurityKey(bytes),
+        ValidAudience = builder.Configuration["Authentication:ValidAudience"],
+        ValidIssuer = builder.Configuration["Authentication:ValidIssuer"],
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,6 +52,6 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllers().RequireAuthorization();
 
 app.Run();
