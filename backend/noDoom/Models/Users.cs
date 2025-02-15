c418efae7fd5bdefa229588eec59f7af3fd670f3
Splix1 <austinTgautney@gmail.com>
2025-02-15 12:28:27 -0800

    created Users model based on Supabase table

---
+++ backend/noDoom/Models/Users.cs
@@ -0,0 +1,20 @@
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+[Table("users")]
+class User : BaseModel 
+{
+
+    [PrimaryKey("id", false)]    
+    public int Id { get; set; }
+
+    [Column("email")]
+    public string Email { get; set; }
+
+    [Column("bluesky_show_following_only")]
+    public bool BlueskyShowFollowingOnly { get; set; } = true;
+
+    [Column("reddit_show_following_only")]
+    public bool RedditShowFollowingOnly { get; set; } = true;
+
+}
