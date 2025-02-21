9d733740a91499b657de951e33c339b8f10e739f
Splix1 <austinTgautney@gmail.com>
2025-02-21 12:29:41 -0800

    condense models

--- backend/noDoom/Models/PostsResponse.cs
+++ backend/noDoom/Models/PostsResponse.cs
@@ -7,19 +7,29 @@ public class PostsResponse
 
 public class PostWithMetrics
 {
-    public string Uri { get; set; }
+    public string PostUri { get; set; }
     public string Cid { get; set; }
-    public int LikeCount { get; set; }
-    public int RepostCount { get; set; }
-    public int ReplyCount { get; set; }
+    public PostAuthor PostAuthor { get; set; }
+    public PostRecord PostRecord { get; set; }
+    public PostMetrics Metrics { get; set; }
+}
+
+public class PostAuthor
+{
+    public string Did { get; set; }
+    public string Handle { get; set; }
+    public string DisplayName { get; set; }
+    public string Avatar { get; set; }
+}
+
+public class PostRecord
+{
+    public string Text { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Embed? Embed { get; set; }
 }
 
 public class PostMetrics
 {
-    public LikeCount? LikeCount { get; set; }
-}
-
-public class LikeCount
-{
-    public int Count { get; set; }
+    public int LikeCount { get; set; }
 } 
