e5e7969a81e84243e5d45657a3ed094bb52b2d59
Splix1 <austinTgautney@gmail.com>
2025-03-20 12:22:13 -0700

    chore: set up standardized mobile project folder structure

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,7 @@
 import 'react-native-url-polyfill/auto'
 import { useState, useEffect } from 'react'
-import { supabase } from './lib/supabase'
-import Auth from './components/Auth'
+import { supabase } from './app/lib/supabase'
+import Auth from './app/components/Auth'
 import { View, Text } from 'react-native'
 import { Session } from '@supabase/supabase-js'
 
